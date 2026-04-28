969bc0f5a3d1deb5cb4aa362bd41cbdaaebd3afa
kakadayan25-pixel <kakadayan25@gmail.com>
2026-04-28 11:34:38 +0700

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,24 @@
+package reviews;
+
+import javax.swing.*;
+
+public class Assignment1 extends JFrame {
+    public Assignment1(){
+        setSize(500,400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new Assignment1();
+    }
+}
+// Bos kru
+// public class JFrameReview extends JFrame{
+//      public JFrameReview(){
+//          setSize(width: 300, heigh: 150);
+//          setLocationRelativeTo(null);
+//  }
+//  public static void main(String[] args){
+//      new JFrameReview();
+//     }
+//  }
